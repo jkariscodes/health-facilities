fef8a39d5b94289a6d757b969519e56b631ebb80
Joseph Kariuki <joehene@gmail.com>
2023-08-20 07:51:23 +0300

    Initialized database settings.

--- health_facilities/settings.py
+++ health_facilities/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "facilities",
+    "leaflet",
+    # "restframework",
+    # "restframework_gis",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = "health_facilities.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "health_facilities",
+        "USER": "jkariuki",
+        "PASSWORD": "H3althFaciliti3s",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
