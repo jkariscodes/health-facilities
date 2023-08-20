9ee508a771eed963990a534098fbf641ca41396a
Joseph Kariuki <joehene@gmail.com>
2023-08-20 07:52:01 +0300

    Added health facilities model.

--- facilities/models.py
+++ facilities/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class HealthFacilites(models.Model):
+    name = models.CharField(max_length=80)
+    healthcare = models.CharField(max_length=167)
+    amenity = models.CharField(max_length=80)
+    operatorty = models.CharField(max_length=80)
+    geom = models.PointField(srid=4326)
